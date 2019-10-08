2f23202ecde0f7da72e282828344027c92b66702
pawardipawesh <dipawesh.iitp@gmail.com>
2019-10-09 00:49:23 +0530

    adding python file

---
+++ compute_mirror_of_input_binary_tree.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Binary_Tree import Binary_Tree
+
+
+# In[3]:
+
+
+root=Binary_Tree(1,None,None)
+root.create_tree()
+
+
+# In[4]:
+
+
+root.inorder_traversal()
+
+
+# In[5]:
+
+
+def create_mirror(root):
+    if root==None:
+        return None
+    
+    if root.left!=None:
+        left_mirrored=create_mirror(root.left)
+    else:
+        left_mirrored=None
+    
+    if root.right!=None:
+        right_mirrored=create_mirror(root.right)
+    else:
+        right_mirrored=None
+    
+    root.left=right_mirrored
+    root.right=left_mirrored
+    
+    return root
+
+
+# In[6]:
+
+
+mirrored_tree_root=create_mirror(root)
+
+
+# In[7]:
+
+
+mirrored_tree_root.inorder_traversal()
+
+
+# In[ ]:
+
+
+
+
